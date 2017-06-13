034af603d724fb5b584ef117c53cc3aaf54473c2
Aluno <tulioassis@outlook.com>
2017-06-13 18:20:39 -0300

    Add

--- ordenar.pl
+++ ordenar.pl
@@ -1,13 +1,10 @@
-ordenar([], X).
-ordenar([A|L1], L) :- menor([A|L1], X), remover(X, [A|L1], R), ordenar(L1, [X|L]).
+ordenar([], []).
+ordenar(L1, [X|L]) :- menor(L1, X), remover(X, L1, R), ordenar(R, L).
     
-concatenar([], L, L).
-concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
-
-remover(X, [X|T], T).
+remover(X, [X|T], T) :- !.
 remover(X, [Y|T], [Y|NT]) :- X \== Y, remover(X, T, NT).
 
 menor([C],C).
 menor([X,Y|V], W) :- X < Y, menor([X|V],W).
-menor([X,Y|V], W) :- menor([Y|V],W).
+menor([_,Y|V], W) :- menor([Y|V],W).
 
